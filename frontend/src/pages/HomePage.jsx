ee13c028d280cff9b51c84aa20d3c70514c23f3d
Anantha Krishnan <ananthak803@gmail.com>
2026-02-04 14:06:42 +0530

    homepage update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Hero from '../components/normal/Hero'
 import About from '../components/normal/About'
+import Feature from '../components/normal/Feature'
+import Team from '../components/normal/Team'
 const HomePage = () => {
   return (
     <div className='relative'>
         <Hero/>
-        <div className='sticky h-[500vh] top-0 bg-white'>
-
+        <div className='sticky  top-0 bg-black'>
     <About/>
+    <Feature/>
+    <Team/>
         </div>
+        
     </div>
   )
 }
