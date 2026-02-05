3d95e849146f35a4ccf63f90d5ff1a3ce635bbb0
Rajneesh Rathore <rajneeshrathore63@gmail.com>
2026-02-05 22:07:14 +0530

    logout feature addition

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import RefreshTokenModel from "../models/refreshToken.model.js";
+import crypto from "crypto";
 import bcrypt from "bcrypt";
 
 const cookieOptions={
@@ -56,10 +57,23 @@ const login = asyncHandler(async (req, res) => {
   if (!isMatch) throw new ApiError(400, "Invalid email or password");
 
   const{ accessToken, refreshToken } = await generateTokens(user, req.headers['user-agent'] || 'Unknown Device');
+
   const loggedInUser = await UserModel.findById(user._id).select("-password");
+
   return res.status(200)
   .cookie("accessToken", accessToken, cookieOptions)
   .cookie("refreshToken", refreshToken, cookieOptions).json(new ApiResponse(200,loggedInUser, "User logged in successfully"));
 });
 
-export { register ,login};
+const logout = asyncHandler(async (req,res)=>{
+   const token = req.cookies?.refreshToken;
+   if(!token) throw new ApiError(401,"Unauthorized");
+   const hashedToken = crypto.createHash("sha256").update(token).digest("hex"); 
+   const refreshToken = await RefreshTokenModel.findOneAndDelete({token: hashedToken});
+   if(!refreshToken) throw new ApiError(403,"Invalid refresh token");
+   res.clearCookie("accessToken", cookieOptions);
+   res.clearCookie("refreshToken", cookieOptions);
+   return res.status(200).json(new ApiResponse(200,{},"Logged out successfully"));
+});
+
+export { register ,login,logout};
