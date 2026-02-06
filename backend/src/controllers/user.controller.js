77fec11d2b96ce381157086b17cfbaeea7f41ae5
Rajneesh Rathore <rajneeshrathore63@gmail.com>
2026-02-06 09:10:46 +0530

    few changes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,12 +1,9 @@
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
-import {UserModel} from "../models/user.model.js";
 
 const getProfile=asyncHandler(async(req,res)=>{
-    // const user=await UserModel.findById(req.user.id).select("-password");
-    // if(!user) throw new ApiError(404,"User not found");
-    return res.json(new ApiResponse(200,"User profile",req.user));
+    return res.json(new ApiResponse(200,req.user,"User profile"));
 });
 
 export {getProfile};
